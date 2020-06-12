cb84bc4b51c823aecbcb42a87ea9031e0be7fc9a
Michal Charemza <michal@charemza.name>
2020-06-12 07:49:34 +0100

    (feat) Port from hawk-server-asyncio

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # hawk-server
+
 Utility function to perform the server-side of Hawk authentication
+
+
+## Installation
+
+```bash
+pip install hawk-server
+```
+
+
+## Usage
+
+```python
+from hawkserver import authenticate_hawk_header
+
+def lookup_credentials(id):
+    # Return {'id': 'some-id', 'key': 'some-secret'} matching credentials,
+    # or None if credentials can't be found
+
+def seen_nonce(nonce, id):
+    # Store nonce, return True if nonce previously seen
+
+is_authenticated, error_message, credentials = authenticate_hawk_header(
+    lookup_credentials, seen_nonce, max_skew,
+    header, method, host, port, path, content_type, content,
+)
+if not is_authenticated:
+    # Return error or raise exception as needed
+```
---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+
+def long_description():
+    with open('README.md', 'r') as file:
+        return file.read()
+
+
+setuptools.setup(
+    name='hawk-server',
+    version='0.0.0',
+    author='Department for International Trade',
+    author_email='webops@digital.trade.gov.uk',
+    description='Utility function to perform the server-side of Hawk authentication',
+    long_description=long_description(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/uktrade/hawk-server',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.7.0',
+    py_modules=[
+        'hawkserver',
+    ],
+    test_suite='test',
+    tests_require=[
+        'freezegun==0.3.12',
+        'mohawk==0.3.4',
+    ],
+)
